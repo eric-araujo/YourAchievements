8e00bee8b06db782eb6baf9f12b318ed234351c4
eric-araujo <eric.araujolima@hotmail.com>
2021-01-02 16:36:38 -0300

    Barra de pesquisa

--- src/Pages/Panel/index.js
+++ src/Pages/Panel/index.js
@@ -1,10 +1,21 @@
 import HeaderPanel from "../../Components/HeaderPanel";
 import { Link } from "react-router-dom";
+import { Search } from 'react-feather';
+
+import "./styles.css";
 
 function Panel() {
     return (
         <div>
             <HeaderPanel />
+            <main className="container">
+                <div className="columnPanel">
+                    <div className="divSearch">
+                        <input type="text" className="search" placeholder="Pesquisar"/>
+                        <button type="button" className="iconSearch"><span><Search /></span></button>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
