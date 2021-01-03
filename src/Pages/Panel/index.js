c38bcc8a7c45d9368a157951049971fe53673703
eric-araujo <eric.araujolima@hotmail.com>
2021-01-03 10:02:38 -0300

    Sessao notfound

--- src/Pages/Panel/index.js
+++ src/Pages/Panel/index.js
@@ -1,6 +1,8 @@
 import HeaderPanel from "../../Components/HeaderPanel";
 import { Link } from "react-router-dom";
-import { Search, Plus } from 'react-feather';
+import { Search, Plus } from "react-feather";
+
+import NotFound from "../../Assets/img/NotFound.svg";
 
 import "./styles.css";
 
@@ -16,6 +18,17 @@ function Panel() {
                     </div>
                     <div className="divGray">
                         <button className="addObjective"><span><Plus /></span>Adicionar Objetivo</button>
+                    </div>
+                    <div className="objectives">
+                        <section className="notfound">
+                            <img src={NotFound} alt="Nenhum resultado encontrado"/>
+                            <h2>Seus Objetivos!</h2>
+                            <p>
+                                <b>Oiiii Eric,</b> aqui é onde você irá colocar os seus objetivos, 
+                                e eu vou ajudar você a tentar alcançar eles. 
+                                Entãoooo... Vamos começar, clique no botão “Adicionar Objetivo”.
+                            </p>
+                        </section>
                     </div>
                 </div>
             </main>
