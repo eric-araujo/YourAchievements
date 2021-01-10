ece63c2818dc111134f4e0682bd0712f7941c08f
eric-araujo <eric.araujolima@hotmail.com>
2021-01-10 15:29:45 -0300

    Criado componente PanelNotFound

---
+++ src/Components/PanelNotFound/index.js
@@ -0,0 +1,19 @@
+import NotFound from "../../Assets/img/NotFound.svg";
+
+import "./styles.css";
+
+const PanelNotFound = () => {
+    return (
+        <section className="notfound">
+            <img src={NotFound} alt="Nenhum resultado encontrado"/>
+            <h2>Seus Objetivos!</h2>
+            <p>
+                <b>Oiiii Eric,</b> aqui é onde você irá colocar os seus objetivos, 
+                e eu vou ajudar você a tentar alcançar eles. 
+                Entãoooo... Vamos começar, clique no botão <b>“Adicionar Objetivo”</b>.
+            </p>
+        </section>
+    );
+}
+
+export default PanelNotFound;
--- src/Pages/Panel/index.js
+++ src/Pages/Panel/index.js
@@ -1,8 +1,7 @@
 import HeaderPanel from "../../Components/HeaderPanel";
+import PanelNotFound from "../../Components/PanelNotFound";
 import { Link } from "react-router-dom";
 import { Search, Plus } from "react-feather";
-
-import NotFound from "../../Assets/img/NotFound.svg";
 
 import "./styles.css";
 
@@ -20,15 +19,7 @@ function Panel() {
                         <button className="addObjective"><span><Plus /></span>Adicionar Objetivo</button>
                     </div>
                     <div className="objectives">
-                        <section className="notfound">
-                            <img src={NotFound} alt="Nenhum resultado encontrado"/>
-                            <h2>Seus Objetivos!</h2>
-                            <p>
-                                <b>Oiiii Eric,</b> aqui é onde você irá colocar os seus objetivos, 
-                                e eu vou ajudar você a tentar alcançar eles. 
-                                Entãoooo... Vamos começar, clique no botão “Adicionar Objetivo”.
-                            </p>
-                        </section>
+                       <PanelNotFound />
                     </div>
                 </div>
             </main>
